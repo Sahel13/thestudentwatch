8577b6a46aebba65476d311ba99f42623ed6684b
Sahel <sahel13miqbal@gmail.com>
2020-06-22 22:44:18 +0530

    Links made more descriptive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import { rhythm, scale } from '../utils/typography'
 const Footer = () => {
     return (
         <SiteFooter>
-            <p>Consider <Link to="/contribute">contributing</Link> to The Student Watch.</p>
-            <p>If you have a suggestion for improving the website, please open a <a href="https://github.com/Sahel13/Sahel13.github.io" target="_blank" rel="noreferrer noopener">Github</a> issue.</p>
+            <p>Consider <Link to="/contribute">contributing to The Student Watch.</Link></p>
+            <p>If you have a suggestion for improving the website, please <a href="https://github.com/Sahel13/Sahel13.github.io" target="_blank" rel="noreferrer noopener">open a Github issue.</a></p>
             <p>This website is licensed under a <a target="_blank" rel="license noreferrer noopener" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>
         </SiteFooter>
     )
